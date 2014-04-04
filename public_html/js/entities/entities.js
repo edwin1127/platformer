5fae3e8f5c631b057a36cf66b6fc8c021eccedcb
edwin1127 <edwin11ramirez@gmail.com>
2014-04-04 13:37:40 -0700

    I have did the trigger
    When i go to the the door it transports me to a different level map.

--- public_html/js/entities/entities.js
+++ public_html/js/entities/entities.js
@@ -44,6 +44,7 @@ game.PlayerEntity = me.ObjectEntity.extend({
                
        onCollision: function(){
            this.collidable = false;
-           me.levelDirector.loadLevel(this.level);
+           me.levelDirector.loadLevel.defer(this.level);
+           me.state.current().resetPlayer.defer();
        }
    }) ;
